c61ce0b00cb9cce8191f8e7ea73c0a11454ec39f
zzl-hyun <kimgihyun877@gmail.com>
2025-03-22 00:00:28 +0900

    25.03.21

---
+++ src/test/java/repository/MemoryMemberRepositoryTest.java
@@ -0,0 +1,23 @@
+package repository;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class MemoryMemberRepositoryTest {
+    MemberRepository repository = new MemoryMemberRepository();
+
+    @Test
+    public void save(){
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        Assertions.assertEquals(member, result);
+
+    }
+}
